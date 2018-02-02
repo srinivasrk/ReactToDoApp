82fd207f5c8b4c5e463f20f12a60671ab788ad1c
srini <srinivasrk@bitbucket.com>
2018-02-02 18:30:59 -0500

    added heroku and added basic code

---
+++ app/components/ToDoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var ToDoApp = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <p> Welcome to TODO App </p>
+      </div>
+    )
+  }
+});
+
+module.exports = ToDoApp;
